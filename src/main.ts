4116690afd29bf3464f9641a945474b6ca4111e4
Negative-hub <and020702@gmail.com>
2022-09-25 00:44:27 +0300

    add first template page

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  NestFastifyApplication,
+  FastifyAdapter,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.setViewEngine({
+    engine: {
+      pug: require('pug'),
+    },
+    templates: join(__dirname, '..', 'views'),
+  });
+
   await app.listen(3000);
 }
 bootstrap();
